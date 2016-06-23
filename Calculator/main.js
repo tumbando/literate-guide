b8f99a09a72b80d151e5771b4053fd562a4b3add
Louie Alarcon <tumbando@gmail.com>
2016-06-23 07:49:56 -0500

    added border boxes and position

---
+++ Calculator/main.js
@@ -0,0 +1,9 @@
+function add(){
+  var firstNumber = document.querySelector('.input1').value;
+  var secondNumber = document.querySelector('.input2').value;
+  var sum = Number(firstNumber) + Number(secondNumber);
+  console.log(sum);
+  document.querySelector('output').innerHTML=sum;
+}
+
+document.querySelector('button').onclick=add;
